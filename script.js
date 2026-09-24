ee5c28e0f8e580bc0a36fcfe2c05c9b9da40a29c
EmanuelleGodinho <manugodinho2009@gmail.com>
2026-09-24 09:15:45 -0400

    Implement button selection for specialties
    Add event listeners to specialty buttons to toggle selection.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("FisioFácil carregado!");
+const botoesEspecialidade = document.querySelectorAll(".especialidades button");
+
+botoesEspecialidade.forEach(function(botao) {
+
+    botao.addEventListener("click", function() {
+
+        botoesEspecialidade.forEach(function(outroBotao) {
+            outroBotao.classList.remove("selecionado");
+        });
+
+        botao.classList.add("selecionado");
+
+    });
+
+});
